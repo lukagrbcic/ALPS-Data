d1e6a999773172bb03341bff909e7b3fcb097a18
Luka <136391682+lukagrbcic@users.noreply.github.com>
2024-06-05 13:54:04 -0700

    Update test_function.py

--- test_function.py
+++ test_function.py
@@ -12,9 +12,8 @@ w = np.linspace(0, 10, 50)
 
 target = bf.targets(benchmark).get_target()
 
-f_ = bf.function(benchmark, target, return_value=True)
+f_ = bf.function(benchmark, target)
 lb, ub = f_.get_bounds()
 
 def f(x):
-    
     return f_.evaluate(x)
